683b2885da29cf508d80ef39f0a899446e59a691
Armend Lesi <contributor4@flatfeestack.io>
2022-02-26 10:47:55 +0100

    add secure api key generation

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -12,13 +13,27 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/test", test).Methods("GET", "OPTIONS")
+	router.HandleFunc("/apikey", createSecureApiKey).Methods("GET", "OPTIONS")
 	log.Printf("listing on port %v", 8000)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(8000), router))
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Test:")
+func test(w http.ResponseWriter, _ *http.Request) {
 	err := json.NewEncoder(w).Encode("Test")
+	if err != nil {
+		return
+	}
+	return
+}
+
+func createSecureApiKey(w http.ResponseWriter, _ *http.Request) {
+	randomBytes := make([]byte, 64)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return
+	}
+	apiKey := base64.StdEncoding.EncodeToString(randomBytes)
+	err = json.NewEncoder(w).Encode(apiKey)
 	if err != nil {
 		return
 	}
